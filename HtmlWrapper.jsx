e4a4ae1791b7dabc547e416af77bba0be82f6690
giladeldor <63351212+giladeldor@users.noreply.github.com>
2023-03-30 14:54:28 +0300

    Changed front page format

---
+++ HtmlWrapper.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HTMLPage() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This is an HTML page rendered with React.</p>
+    </div>
+  );
+}
+
+export default HTMLPage;

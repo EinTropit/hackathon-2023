8a19d8fa237fb60225a9ef5653866a90fba7aed8
giladeldor <63351212+giladeldor@users.noreply.github.com>
2023-03-30 22:56:49 +0300

    Added things

--- Volu/js/volunteer.js
+++ Volu/js/volunteer.js
@@ -1,5 +1,14 @@
 function openPopup() {
-    var popup = window.open("", "myPopup", "width=500,height=500");
-    popup.document.write("<textarea rows='10' cols='50'></textarea>");
-  }
-  
+    var now = new Date();
+    var popupName = "myPopup" + now.getTime();
+    var popup = window.open("", popupName, "width=500,height=500");
+    popup.document.write("<form>");
+    popup.document.write("<label for='name'>Name:</label><br>");
+    popup.document.write("<input type='text' id='name' name='name'><br><br>");
+    popup.document.write("<label for='availability'>Availability:</label><br>");
+    popup.document.write("<input type='text' id='availability' name='availability'><br><br>");
+    popup.document.write("<label for='profession'>Profession:</label><br>");
+    popup.document.write("<input type='text' id='profession' name='profession'><br><br>");
+    popup.document.write("<input type='submit' value='Submit' onclick='window.close();'>");
+    popup.document.write("</form>");
+}
